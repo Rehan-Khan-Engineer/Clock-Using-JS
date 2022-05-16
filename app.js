acee188592a1db52a7343e2a0468a351f4eec314
Rehan-Khan-Engineer <rk116159@gmail.com>
2022-05-16 20:34:33 +0530

    added-12-&-24-hour-format

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-startButton = document.getElementById("start-btn");
+const startButton = document.getElementById("start-btn");
 
-stopButton = document.getElementById("stop-btn");
+const stopButton = document.getElementById("stop-btn");
 
 startButton.addEventListener("click", getTime);
 
@@ -9,18 +9,41 @@ stopButton.addEventListener("click", stopTime);
 let stopClock = null;
 
 function getTime() {
+  const getFormat = document.getElementById("selectBox");
   const time = new Date();
 
   let hours = time.getHours();
   let minutes = time.getMinutes();
   let seconds = time.getSeconds();
+  let session = "AM";
 
   minutes = addZero(minutes);
   seconds = addZero(seconds);
 
+  if (getFormat.options[getFormat.selectedIndex].value === "12 Hour clock") {
+    if (hours === 0) {
+      hours = 12;
+    }
+
+    if (hours > 12) {
+      hours = hours - 12;
+      session = "PM";
+    }
+
+    console.log("working for 12 format");
+  } else if (
+    getFormat.options[getFormat.selectedIndex].value === "24 Hour clock"
+  ) {
+    if (hours > 12) {
+      session = "PM";
+    }
+
+    console.log("working for 24 format");
+  }
+
   const getClock = document.getElementById("clock");
   getClock.style.display = "block";
-  getClock.innerHTML = `⌚ ${hours} : ${minutes} : ${seconds}`;
+  getClock.innerHTML = `⌚ ${hours} : ${minutes} : ${seconds} ${session}`;
 
   stopClock = setTimeout(getTime, 1000);
 }
